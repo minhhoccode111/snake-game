77126670834ffc21072606809a990c42fb69504a
minhhoccode111 <minhhoccode111@gmail.com>
2023-06-06 23:15:21 +0700

    Conver ES6 Modules to the module pattern

--- game.js
+++ game.js
@@ -1,28 +1,49 @@
-import { onSnake, expandSnake } from "./snake.js";
-import { randomGridPosition } from "./grid.js";
+import {
+  update as updateSnake,
+  draw as drawSnake,
+  SNAKE_SPEED,
+  getSnakeHead,
+  snakeIntersection,
+} from "./snake.js";
+import { update as updateFood, draw as drawFood } from "./food.js";
+import { outsideGrid } from "./grid.js";
 
-let food = getRandomFoodPosition();
-const EXPANSION_RATE = 5;
+let lastRenderTime = 0;
+let gameOver = false;
+const gameBoard = document.getElementById("game-board");
 
-export function update() {
-  if (onSnake(food)) {
-    expandSnake(EXPANSION_RATE);
-    food = getRandomFoodPosition();
+function main(currentTime) {
+  if (gameOver) {
+    if (confirm("You lost. Press ok to restart.")) {
+      window.location = "/";
+    }
+    return;
   }
+
+  window.requestAnimationFrame(main);
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+
+  lastRenderTime = currentTime;
+
+  update();
+  draw();
 }
 
-export function draw(gameBoard) {
-  const foodElement = document.createElement("div");
-  foodElement.style.gridRowStart = food.y;
-  foodElement.style.gridColumnStart = food.x;
-  foodElement.classList.add("food");
-  gameBoard.appendChild(foodElement);
+window.requestAnimationFrame(main);
+
+function update() {
+  updateSnake();
+  updateFood();
+  checkDeath();
 }
 
-function getRandomFoodPosition() {
-  let newFoodPosition;
-  while (newFoodPosition == null || onSnake(newFoodPosition)) {
-    newFoodPosition = randomGridPosition();
-  }
-  return newFoodPosition;
+function draw() {
+  gameBoard.innerHTML = "";
+  drawSnake(gameBoard);
+  drawFood(gameBoard);
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
 }
