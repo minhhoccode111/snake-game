2c6e6a43f9adb6b847fd88eed4cdac214eaf0988
minhhoccode111 <minhhoccode111@gmail.com>
2023-07-16 00:59:28 +0700

    Finished project

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 A basic Nokia Snake Game with HTML, CSS, JAVASCRIPT to practice OOP in JAVASCRIPT
 
+## What I've learned
+
+- OOP Single Responsibility
+- `setInterval`
+- `window.confirm()`
+- `window.location.href='/'`
+- object destructuring `const setDirection = ({ x, y }) => {}`
+- `requestAnimationFrame`
+
 [View all my projects Live demo links](https://minhhoccode111.github.io/allProjectssLiveDemo/)
 
 [View this project Live demo](https://minhhoccode111.github.io/snakeGame/)
--- app.js
+++ app.js
@@ -18,11 +18,11 @@ const Cell = (xAxis, yAxis, type) => {
   const change = (newX, newY) => {
     x = newX;
     y = newY;
+    // make it clarify instead of css shorthand
     div.style.gridColumnStart = `${x}`;
     div.style.gridColumnEnd = `${x + 1}`;
     div.style.gridRowStart = `${y}`;
     div.style.gridRowEnd = `${y + 1}`;
-    // div.style.gridArea = `${x}/${y}/${x + 1}/${y + 1}`;
   };
 
   return {
@@ -62,11 +62,10 @@ const grid = (() => {
 })();
 
 const snake = (() => {
-  let speed = 750; // ms refresh frame
-  let speedExtra = 5; // 1000ms - 5ms each time snake eat
-  let growRate = 1; // + 1 Cell each time snake eat
-  let cellsToGrow = 0; //while this === 0, the snake can move, while this !== 0, the snake grows instead of moving and each time the snake grow cellsToGrow--
-  // when the snake eat something, cellsToGrow += growRate
+  let speed = 100; // ms refresh frame
+  let speedExtra = 245; // 750ms - 25ms each time snake eat
+  let growRate = 2; // + 1 Cell each time snake eat
+  let cellsToGrow = 0; //while this === 0, the snake can move, while this !== 0, the snake grows instead of moving (but grow ahead of snake's head so it still look like moving) and each time the snake grow cellsToGrow--
 
   let direction = { x: 1, y: 0 }; // init move right
 
@@ -74,12 +73,16 @@ const snake = (() => {
 
   const getDirection = () => direction;
 
-  const setDirection = (obj) => {
-    direction.x = obj.x;
-    direction.y = obj.y;
+  const setDirection = ({ x, y }) => {
+    direction.x = x;
+    direction.y = y;
   };
 
-  const grow = () => (cellsToGrow += growRate);
+  // grow is call after snake ate food
+  const grow = () => {
+    cellsToGrow += growRate; // + cells
+    speed -= speedExtra; // - refresh rate
+  };
 
   // movable direction is not current direction and not opposite to current direction
   const movableDirection = (obj) => {
@@ -92,23 +95,27 @@ const snake = (() => {
     return false;
   };
 
+  // init body
   const body = [Cell(10, 10, 'snake'), Cell(9, 10, 'snake'), Cell(8, 10, 'snake'), Cell(7, 10, 'snake'), Cell(6, 10, 'snake')];
 
   // to check if its head is on body (or food)
   const isOnBody = (obj) => {
-    return body.some((current, index) => {
-      // start from index 1 because we already have isOnHead
-      if (index === 0) return;
+    // start from index 1 because we already have isOnHead
+    return body.slice(1).some((current, index) => {
+      // we'll check on body with snake's head (which is a cell)
+      // or with food (so that we don't respawn food on snake's body, and food is a cell too)
+      // 'cause the are both cells so we use getX() and getY()
       return obj.getX() === current.getX() && obj.getY() === current.getY();
     });
   };
 
   const getHead = () => body[0];
 
+  // is used to check if food (which is a cell) is on head
   const isOnHead = (obj) => getHead().getX() === obj.getX() && getHead().getY() === obj.getY();
 
   const move = () => {
-    // move it ahead of snake's head base on current direction to make it moving
+    // move its tail ahead of snake's head base on current direction to make it moving
     const aheadX = getHead().getX() + direction.x;
     const aheadY = getHead().getY() + direction.y;
     //if we have cells to grow then we grow before moving
@@ -118,11 +125,11 @@ const snake = (() => {
       cellsToGrow--;
       return;
     }
-    // take last cell
+    // else we take last cell
     const last = body.pop();
-    // change cell's position
+    // change last cell's position to be ahead of snake's head
     last.change(aheadX, aheadY);
-    // make last cell to be first cell in body array
+    // make last cell to be the front
     body.unshift(last);
   };
 
@@ -140,10 +147,21 @@ const snake = (() => {
 })();
 
 const food = (() => {
-  let position = Cell(15, 15, 'food');
+  const position = Cell(15, 15, 'food');
 
   const change = () => {
-    //TODO FIXME implement spawn food
+    // use grid method to get random position
+    let random = grid.randomPosition();
+
+    // change food position to that random spot
+    position.change(random.x, random.y);
+
+    // while (if) new position isOnHead or isOnBody of snake
+    while (snake.isOnHead(position) || snake.isOnBody(position)) {
+      // do it again
+      random = grid.randomPosition();
+      position.change(random.x, random.y);
+    }
   };
 
   const getPosition = () => position;
@@ -156,20 +174,28 @@ const food = (() => {
 
 const game = (() => {
   const start = () => {
+    // game start then snake start moving
     const moving = setInterval(snakeMove, snake.getSpeed());
 
     function snakeMove() {
       snake.move();
-      // check game ended
+      // check game ended when snake's head outside of grid
+      // or snake's head is on its body
       if (grid.checkOutside(snake.getHead()) || snake.isOnBody(snake.getHead())) {
+        // then clear interval
         clearInterval(moving);
+        // ask to play again
         if (window.confirm('Play again?')) {
+          // if yes then reload
           window.location.href = '/';
         }
       }
-      // check snake eat food
+      // check if snake eat food
       if (snake.isOnHead(food.getPosition())) {
+        // then snake grow
         snake.grow();
+        // spawn food on new position
+        food.change();
       }
     }
   };
@@ -177,7 +203,8 @@ const game = (() => {
   return { start };
 })();
 
-const listener = (() => {
+// event listeners
+(() => {
   const letters = {
     ArrowUp: { x: 0, y: -1 },
     ArrowLeft: { x: -1, y: 0 },
@@ -186,8 +213,11 @@ const listener = (() => {
   };
 
   window.addEventListener('keyup', (e) => {
+    // check if right keys is pressed
     if (letters.hasOwnProperty(e.key)) {
+      // then check if that key is movable direction
       if (snake.movableDirection(letters[e.key])) {
+        // then change snake's direction
         snake.setDirection(letters[e.key]);
       }
     }
